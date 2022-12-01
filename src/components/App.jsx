d05e3388f4011f1748353a5afe6e0e7722af9dfa
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-12-01 17:11:19 +0200

    add component filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,30 @@ import { nanoid } from 'nanoid'
 import Box from "./Box";
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
 
+const contacts = [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+                  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+                  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+                  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+                 ]
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    
+    contacts: contacts,
+    filter: ''
   }
 
-  deleteContact = contactId => {
+  handleDeleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId)
     }))
   }
 
-  addContact = name => {
+  addContact = ({name, number}) => {
     const contact = {
       id: nanoid(),
       name,
+      number
     }
 
     this.setState(prevState => ({
@@ -32,11 +34,20 @@ export class App extends Component {
     }))
   };
  
+  handleChangeFilter = e => {
+    this.setState({ filter: e.target.value });
+  }
 
   
 
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state
+    
+    const normalizedFilter = this.state.filter.toLowerCase();
+    
+    const filterContacts = this.state.contacts.filter(contact => {
+     return contact.name.toLowerCase().includes(normalizedFilter)
+    });
 
     return <Box
       height='100vh'
@@ -50,7 +61,8 @@ export class App extends Component {
       <h1>Phonebook</h1>
       <ContactForm onSubmit={this.addContact} />
       <h2>Contact</h2>
-      <ContactList contacts={contacts} deleteContact={this.deleteContact} />
+      <Filter value={filter} onChange={this.handleChangeFilter} />
+      <ContactList contacts={filterContacts} onDeleteContact={this.handleDeleteContact} />
     </Box>
   };
 };
